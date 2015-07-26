52d45fcd161d9ca9bee64302923018eefe4bb0a8
kaushal087 <kaushal10087@gmail.com>
2015-07-26 23:01:37 +0530

    Added UI files

---
+++ js/slide-menu.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('#slide-nav.navbar .container').append($('<div id="navbar-height-col"></div>'));
+    var toggler = '.navbar-toggle';
+    var pagewrapper = '#page-content';
+    var navigationwrapper = '.navbar-header';
+    var menuwidth = '100%'; 
+    var slidewidth = '80%';
+    var menuneg = '-100%';
+    var slideneg = '-80%';
+    $("#slide-nav").on("click", toggler, function (e) {
+        var selected = $(this).hasClass('slide-active');
+        $('#slidemenu').stop().animate({
+            left: selected ? menuneg : '0px'
+        });
+        $('#navbar-height-col').stop().animate({
+            left: selected ? slideneg : '0px'
+        });
+        $(pagewrapper).stop().animate({
+            left: selected ? '0px' : slidewidth
+        });
+        $(navigationwrapper).stop().animate({
+            left: selected ? '0px' : slidewidth
+        });
+        $(this).toggleClass('slide-active', !selected);
+        $('#slidemenu').toggleClass('slide-active');
+        $('#page-content, .navbar, body, .navbar-header').toggleClass('slide-active');
+    });
+    var selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
+    $(window).on("resize", function () {
+        if ($(window).width() > 767 && $('.navbar-toggle').is(':hidden')) {
+            $(selected).removeClass('slide-active');
+        }
+    });
+});
